b66afd9a4b8276e962d38af730db5560f445f419
jgeddes3 <59842820+jgeddes3@users.noreply.github.com>
2024-06-03 15:21:15 -0500

    Stickybar fixed and lookin good

--- src/Components/ForEveryPage/Stickybar.js
+++ src/Components/ForEveryPage/Stickybar.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import './StickyBar.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const StickyBar = () => {
-  const navigateTo = (path) => {
-    window.location.href = path;
-  };
+  const location = useLocation();
 
   useEffect(() => {
     const scrollFunc = () => {
+      const header = document.getElementById('sticky-header');
       const buttons = document.querySelectorAll('.nav-btn');
       if (window.scrollY >= 240) {
+        header.classList.add('scrolled');
         buttons.forEach((btn) => btn.classList.add('scrolled'));
       } else {
+        header.classList.remove('scrolled');
         buttons.forEach((btn) => btn.classList.remove('scrolled'));
       }
     };
@@ -23,14 +24,13 @@ const StickyBar = () => {
 
   return (
     <header id="sticky-header">
-    <Link to="/" className="nav-btn" id="home-btn">Home</Link>
-    <Link to="/about" className="nav-btn" id="about-btn">About Me</Link>
-    <Link to="/projects" className="nav-btn" id="projects-btn">Projects</Link>
-    <Link to="/contact" className="nav-btn" id="contact-btn">Contact Me</Link>
-    <Link to="/misc" className="nav-btn" id="misc-btn">Misc</Link>
-  </header>
-);
-
+      <Link to="/" className={`nav-btn ${location.pathname === '/' ? 'active' : ''}`} id="home-btn">Home</Link>
+      <Link to="/about" className={`nav-btn ${location.pathname === '/about' ? 'active' : ''}`} id="about-btn">About Me</Link>
+      <Link to="/projects" className={`nav-btn ${location.pathname === '/projects' ? 'active' : ''}`} id="projects-btn">Projects</Link>
+      <Link to="/contact" className={`nav-btn ${location.pathname === '/contact' ? 'active' : ''}`} id="contact-btn">Contact Me</Link>
+      <Link to="/misc" className={`nav-btn ${location.pathname === '/misc' ? 'active' : ''}`} id="misc-btn">Misc</Link>
+    </header>
+  );
 };
 
 export default StickyBar;
