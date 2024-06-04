e34beac0dd44f61014c80292a799aa1fdfc09368
jgeddes3 <59842820+jgeddes3@users.noreply.github.com>
2024-06-04 09:49:41 -0500

    Saving then restarting

--- src/Components/Pages/ProjectsPage.js
+++ src/Components/Pages/ProjectsPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Background from '../ForEveryPage/Background';
+import Bottombar from '../ForEveryPage/Bottombar';
 
 const ProjectsPage = () => {
   return (
+    <>
+    <Background />
     <div>
       <h1>Projects</h1>
       <p>This is the Projects page.</p>
     </div>
+    <Bottombar />
+    </>
   );
 };
 
