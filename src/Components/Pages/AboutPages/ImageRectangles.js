a0ad44f3a6bba311afb75fa583ba53f7129559ea
jgeddes3 <59842820+jgeddes3@users.noreply.github.com>
2024-09-11 19:55:32 -0500

    About Me Fixed

--- src/Components/Pages/AboutPages/ImageRectangles.js
+++ src/Components/Pages/AboutPages/ImageRectangles.js
@@ -1,28 +1,35 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import img1 from './img1.jpg';
-import img2 from './img2.jpg';
-import img3 from './img3.jpg';
-import img4 from './img4.jpg';
-import img5 from './img5.jpg';
-import img6 from './img6.jpg';
-import img7 from './img7.jpg';
-import img8 from './img8.jpg';
-import img9 from './img9.jpg';
-import img10 from './img10.jpg';
-import img11 from './img11.jpg';
-import img12 from './img12.jpg';
-import img13 from './img13.jpg';
-import img14 from './img14.jpg';
-import img15 from './img15.jpg';
-import img16 from './img16.jpg';
-import img17 from './img17.jpg';
-import img18 from './img18.jpg';
-import img19 from './img19.jpg';
-import img20 from './img20.jpg';
+import img1 from './Lilys.jpg';
+import img2 from './Lisbon, Portugal.jpg';
+import img3 from './Minneapolis, MN.jpg';
+import img4 from './Montrose Point, Chicago.jpg';
+import img5 from './Moorish Castle.jpg';
+import img6 from './Pheonix, Arizona.jpg';
+import img7 from './Portland, Maine.jpg';
+import img8 from './Porto, Portugal.jpg';
+import img9 from './Quinta da Regaleira.jpg';
+import img10 from './Rossaveel, Ireland.jpg';
+import img11 from './San Francisco, California.jpg';
+import img12 from './The Half Blood Prince.jpg';
+import img13 from './Toca da Raposa.jpg';
+import img14 from './Unknown Hills, Ireland.jpg';
+import img15 from './Well Out of Hell.jpg';
+import img16 from './Wicklow Mountains, Ireland.jpg';
+import img17 from './Wolf Point Chicago.jpg';
+import img18 from './Wrigly Field, Chicago.jpg';
+import img19 from './Boyne Mountain, Michigan.jpg';
+import img20 from './Chattanooga, Tennessee.jpg';
+import img21 from './Cliffs of Moher, Ireland.jpg';
+import img22 from './Crete, Greece.jpg';
+import img23 from './Dublin, Ireland.jpg';
+import img24 from './Duluth, Minnesota.jpg';
+import img25 from './Fort Lauderdale, Florida.jpg';
+import img26 from './Galway, Ireland.jpg';
+
 import './ImageRectangles.css';
 
 const ImageRectangles = () => {
-  const images = useMemo(() => [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20], []);
+  const images = useMemo(() => [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26], []);
 
   const [randomImages, setRandomImages] = useState([]);
 
