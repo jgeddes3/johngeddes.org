7bcefe645b209c5f4b3f379927f799af1b674e4f
jgeddes3 <59842820+jgeddes3@users.noreply.github.com>
2024-09-16 21:38:37 -0500

    Finished the review

--- src/Components/Pages/MiscPages/MythOfSisyphusReview.js
+++ src/Components/Pages/MiscPages/MythOfSisyphusReview.js
@@ -34,7 +34,7 @@ const MythOfSisyphusReview = () => {
         <div className="author-links">
           <p>
             Written by John Geddes | "
-            <a href="https://www.goodreads.com/book/show/7235533-the-way-of-kings">The Way of Kings</a> by <a href="https://www.goodreads.com/author/show/38550.Brandon_Sanderson">Brandon Sanderson"</a><br/>
+            <a href="https://www.goodreads.com/book/show/11987.The_Myth_of_Sisyphus_and_Other_Essays">The Myth of Sisyphus and Other Essays</a> by <a href="https://www.goodreads.com/author/show/957894.Albert_Camus">Albert Camus</a><br/>
           </p>
         </div>
        
@@ -42,14 +42,12 @@ const MythOfSisyphusReview = () => {
         <div className="paragraph-photo-right-container">
         <div className = "Seperator-photo-right">
           <p>
-          Brandon Sanderson does it again. 
-          Sucks me into a massive series only for it to be probably my favorite fantasy series I’ve read to this date. 
-          To tell the story about how I found it really just shows how the smallest things can have such a drastic impact on your life. 
-          I was at a my sisters 49ers vs Chiefs super bowl party talking with people I have never met and I will never talk to again and this guy brings up Red Rising. 
-          Now the Red Rising series is my favorite and will probably always be my favorite Science Fiction book, so this perked my ears right up. 
-          We got to talking and I recommended him some books (name of the wind) and he recommended me this book right here. 
+          In The Myth of Sisyphus, Albert Camus dives deep into the human condition, exploring themes of absurdity, meaning, and existence. 
+          Camus presents the ancient Greek myth of Sisyphus, the man condemned by the gods to roll a boulder up a hill for eternity, only to watch it roll back down every time he reaches the top. 
+          This endless, futile task becomes a symbol of human life, which Camus argues is inherently devoid of meaning, yet we continue to seek purpose in a world that offers none.
           </p>
-          <p>Initially, I didn’t even think I would read it, but I bought it and it didn’t speak to me. Not at all, I just couldn't relate to Kal while he was in Amarams </p>
+          <p>What makes this myth particularly tragic, Camus suggests, is that Sisyphus is fully aware of his situation. It’s this consciousness that both burdens and frees him. As Camus writes, "If this myth is tragic, it is because the Hero is conscious." Sisyphus knows the futility of his task, yet he 
+          </p>
           </div>
           <div className = "Cover-with-caption">
           <img src={MythOfSisyphus} alt="Way of Kings" />
@@ -58,14 +56,26 @@ const MythOfSisyphusReview = () => {
         </div>
         <div className="paragraph-wrap">
           <p>
-          army. But I stuck with it and really got into it when he became a slave. I think this is what you would call a slow start but the moment it picks up I couldn’t put it down. The attention to detail is crazy, and I am currently writing this review after I finished Rhythm of war, the amount of call backs is insane. I could only read this series for a year and I don’t think I would ever get bored.
+         continues, and it’s in this perseverance that we find a profound statement about human resilience and defiance.
           </p>
         </div>
         
         {/* Paragraph Only Container */}
         <div className="paragraph-only-container">
           <p>
-          The only downfall I’ve heard people say to me that I do get is the book is long. I don’t mind this in the least bit. There are very few things I would cut. I do understand this criticism however, it can seem daunting to read a book that long especially when you start. My only solace is it is so worth it to finish the series.
+          One of the most striking moments in Camus' interpretation is when he emphasizes Sisyphus' descent from the summit. Camus argues that the hour-long walk down, after the rock has inevitably tumbled back, is when Sisyphus is truly free. This descent, often seen as the lowest point, is also a moment of liberation. Camus provocatively suggests that "if this descent is in sorrow, then it can also be in joy." Sisyphus may be condemned to repeat the same task, but in those moments between the struggle, he is conscious of his fate and free to choose his attitude towards it. This is where Camus' existential philosophy shines: the recognition that even in a life devoid of inherent meaning, we can find freedom by accepting and embracing the absurdity.
+          </p>
+        </div>
+        <div className="paragraph-only-container">
+          <p>
+            There is catharsis in monotony and labor, and for that there is mercy involving Sisyphus. Where some are involved in endless suffering at the hands of tormentors, Sisyphus must only push a boulder up a hill. He must simply endure.
+
+          </p>
+        </div>
+        <div className="paragraph-only-container">
+          <p>
+            While I read this book with the lens of my own, I also gained catharsis. While in this endless endeavor of labor and searching for meaning, it feels better knowing that there are others. Others that are laboring endlessly with no end in sight. While I do not know if this was Camus intention, it does make me feel less alone. 
+
           </p>
         </div>
 
